cbf78c924487cd3ea2f45a22d4cae2a7432655d7
Eric Freitas Avelino <ericfravelino@gmail.com>
2024-04-17 16:36:11 -0300

    Starting to implement Board and Piece

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -6,7 +6,6 @@ import boardgame.Position;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Program {
     public static void main(String[] args) {
-        Position pos = new Position(3,5);
-        System.out.println(pos);
+        Board board = new Board(8,8);
     }
 }
---
+++ src/boardgame/Piece.java
@@ -0,0 +1,16 @@
+package boardgame;
+
+public class Piece {
+    protected Position position;
+    private Board board;
+
+    public Piece(Position position, Board board) {
+        this.position = position;
+        this.board = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+}
