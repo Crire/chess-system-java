5fbafd71b397f98655837cc041557aeee35ab631
Eric Freitas Avelino <ericfravelino@gmail.com>
2024-04-18 14:43:48 -0300

    funcionando com o rook e o knight

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -1,4 +1,18 @@
 package chess;
 
-public class ChessPiece {
+import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
+
+public class ChessPiece extends Piece {
+    private Color color;
+
+    public ChessPiece(Board board, Color color) {
+        super(board);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
